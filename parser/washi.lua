413e7023fb8a25720109c55b09d2bd6da3e29163
Jesper Bergström <jesper.b96@gmail.com>
2022-11-20 10:24:30 +0100

    Adds lua files

---
+++ parser/washi.lua
@@ -0,0 +1,25 @@
+function file_exists(file)
+    local f = io.open(file, "rb")
+    if f then
+        f:close()
+    end
+    return f ~= nil
+end
+
+function lines_from(file)
+    if not file_exists(file) then
+        return {}
+    end
+    local lines = {}
+    for line in io.lines(file) do
+        lines[#lines + 1] = line
+    end
+    return lines
+end
+
+local file = 'washi.config'
+local lines = lines_from(file)
+
+for k, v in pairs(lines) do
+    print(v)
+end
